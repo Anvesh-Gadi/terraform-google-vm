2d6a0a38f6009d03e56bb2462b6d180cf9b70eff
pk <134143995+ramrpk5@users.noreply.github.com>
2024-08-29 00:31:06 +0530

    Update variables.tf

--- examples/compute_instance/simple/variables.tf
+++ examples/compute_instance/simple/variables.tf
@@ -1,6 +1,7 @@
 variable "project_id" {
   description = "The ID of the Google Cloud project where resources will be created."
   type        = string
+  default     = "linear-elf-433407-q0"
 }
 
 variable "instance_name" {
