ff9d3f7ccd003c53583aedc01e90151538cd8dfc
pk <134143995+ramrpk5@users.noreply.github.com>
2024-08-29 00:27:43 +0530

    Update main.tf

--- examples/compute_instance/simple/main.tf
+++ examples/compute_instance/simple/main.tf
@@ -1,64 +1,62 @@
-# This code is compatible with Terraform 4.25.0 and versions that are backwards compatible to 4.25.0.
-# For information about validating this Terraform code, see https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#format-and-validate-the-configuration
 resource "google_compute_instance" "instance-20240828-181806" {
-  project      = var.project_id  # Add this line
+  project      = var.project_id
   name         = var.instance_name
   machine_type = var.machine_type
   zone         = var.zone
+
   boot_disk {
     auto_delete = true
-    device_name = "instance-20240828-181806"
+    device_name = var.instance_name
 
     initialize_params {
-      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20240815"
-      size  = 10
-      type  = "pd-balanced"
+      image = var.boot_disk_image
+      size  = var.boot_disk_size
+      type  = var.boot_disk_type
     }
 
     mode = "READ_WRITE"
   }
 
-  can_ip_forward      = false
-  deletion_protection = false
-  enable_display      = false
+  can_ip_forward      = var.can_ip_forward
+  deletion_protection = var.deletion_protection
+  enable_display      = var.enable_display
 
-  labels = {
-    goog-ec-src = "vm_add-tf"
-  }
-
-  machine_type = "e2-medium"
-  name         = "instance-20240828-181806"
+  labels = var.instance_labels
 
   network_interface {
     access_config {
       network_tier = "PREMIUM"
     }
 
-    queue_count = 0
-    stack_type  = "IPV4_ONLY"
-    subnetwork  = "projects/linear-elf-433407-q0/regions/us-central1/subnetworks/default"
+    subnetwork = var.network_subnetwork
   }
 
   scheduling {
-    automatic_restart   = true
-    on_host_maintenance = "MIGRATE"
-    preemptible         = false
-    provisioning_model  = "STANDARD"
+    automatic_restart   = var.automatic_restart
+    on_host_maintenance = var.on_host_maintenance
+    preemptible         = var.preemptible
+    provisioning_model  = var.provisioning_model
   }
 
   service_account {
-    email  = "467144775560-compute@developer.gserviceaccount.com"
-    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+    email  = var.service_account_email
+    scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring.write",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append"
+    ]
   }
 
   shielded_instance_config {
-    enable_integrity_monitoring = true
-    enable_secure_boot          = false
-    enable_vtpm                 = true
+    enable_integrity_monitoring = var.shielded_integrity_monitoring
+    enable_secure_boot          = var.shielded_secure_boot
+    enable_vtpm                 = var.shielded_vtpm
   }
-
-  zone = "us-central1-c"
 }
+
 terraform {
   backend "http" {
     address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/ContinuousIntegration/workspaces/prasannaworkspace/terraform-backend?accountIdentifier=ucHySz2jQKKWQweZdXyCog"
