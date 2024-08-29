10c06138e6f2f5c2e4d5ec54aac75c95cdde7a9c
Anvesh-Gadi <155067334+Anvesh-Gadi@users.noreply.github.com>
2024-08-29 17:14:33 +0530

    Update main.tf

--- examples/compute_instance/simple/main.tf
+++ examples/compute_instance/simple/main.tf
@@ -1,5 +1,6 @@
-resource "google_compute_instance" "instance-20240828-181806" {
-  project      = var.project_id
+# main.tf
+
+resource "google_compute_instance" "instance" {
   name         = var.instance_name
   machine_type = var.machine_type
   zone         = var.zone
@@ -17,18 +18,22 @@ resource "google_compute_instance" "instance-20240828-181806" {
     mode = "READ_WRITE"
   }
 
-  can_ip_forward      = var.can_ip_forward
-  deletion_protection = var.deletion_protection
-  enable_display      = var.enable_display
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
 
-  labels = var.instance_labels
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
 
   network_interface {
     access_config {
-      network_tier = "PREMIUM"
+      network_tier = var.network_tier
     }
 
-    subnetwork = var.network_subnetwork
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = var.subnetwork
   }
 
   scheduling {
@@ -40,30 +45,23 @@ resource "google_compute_instance" "instance-20240828-181806" {
 
   service_account {
     email  = var.service_account_email
-    scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring.write",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/trace.append"
-    ]
+    scopes = var.service_account_scopes
   }
 
   shielded_instance_config {
-    enable_integrity_monitoring = var.shielded_integrity_monitoring
-    enable_secure_boot          = var.shielded_secure_boot
-    enable_vtpm                 = var.shielded_vtpm
+    enable_integrity_monitoring = var.enable_integrity_monitoring
+    enable_secure_boot          = var.enable_secure_boot
+    enable_vtpm                 = var.enable_vtpm
   }
 }
 
 terraform {
   backend "http" {
-    address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/ContinuousIntegration/workspaces/prasannaworkspace/terraform-backend?accountIdentifier=ucHySz2jQKKWQweZdXyCog"
+    address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/ContinuousIntegration/workspaces/anveshiac/terraform-backend?accountIdentifier=ucHySz2jQKKWQweZdXyCog"
     username = "harness"
-    lock_address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/ContinuousIntegration/workspaces/prasannaworkspace/terraform-backend/lock?accountIdentifier=ucHySz2jQKKWQweZdXyCog"
+    lock_address = "https://https://app.harness.io/gateway/iacm/api/orgs/default/projects/ContinuousIntegration/workspaces/anveshiac/terraform-backend/lock?accountIdentifier=ucHySz2jQKKWQweZdXyCog"
     lock_method = "POST"
-    unlock_address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/ContinuousIntegration/workspaces/prasannaworkspace/terraform-backend/lock?accountIdentifier=ucHySz2jQKKWQweZdXyCog"
+    unlock_address = "https://https://app.harness.io/gateway/iacm/api/orgs/default/projects/ContinuousIntegration/workspaces/anveshiac/terraform-backend/lock?accountIdentifier=ucHySz2jQKKWQweZdXyCog"
     unlock_method = "DELETE"
   }
 }
