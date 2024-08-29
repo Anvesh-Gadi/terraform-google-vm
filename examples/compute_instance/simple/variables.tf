1fae8041b50cde73153710c39df07ba18959f019
Anvesh-Gadi <155067334+Anvesh-Gadi@users.noreply.github.com>
2024-08-29 17:10:44 +0530

    Update variables.tf

--- examples/compute_instance/simple/variables.tf
+++ examples/compute_instance/simple/variables.tf
@@ -1,17 +1,23 @@
+# variables.tf
 variable "project_id" {
   description = "The ID of the Google Cloud project where resources will be created."
   type        = string
-  default     = "linear-elf-433407-q0"
-}
+  default     = "centered-loader-422210-p3"
 
 variable "instance_name" {
-  description = "The name of the Google Compute Engine instance."
+  description = "The name of the compute instance."
   type        = string
-  default     = "instance-20240828-181806"
+  default     = "instance-20240829-112235"
+}
+
+variable "machine_type" {
+  description = "The machine type of the compute instance."
+  type        = string
+  default     = "e2-medium"
 }
 
 variable "boot_disk_image" {
-  description = "The image used for the boot disk."
+  description = "The image to use for the boot disk."
   type        = string
   default     = "projects/debian-cloud/global/images/debian-12-bookworm-v20240815"
 }
@@ -28,70 +34,32 @@ variable "boot_disk_type" {
   default     = "pd-balanced"
 }
 
-variable "machine_type" {
-  description = "The machine type for the Google Compute Engine instance."
+variable "subnetwork" {
+  description = "The subnetwork to attach the network interface to."
   type        = string
-  default     = "e2-medium"
+  default     = "projects/centered-loader-422210-p3/regions/us-central1/subnetworks/default"
 }
 
-variable "network_subnetwork" {
-  description = "The subnetwork to which the instance is connected."
+variable "network_tier" {
+  description = "The network tier for the instance."
   type        = string
-  default     = "projects/linear-elf-433407-q0/regions/us-central1/subnetworks/default"
-}
-
-variable "service_account_email" {
-  description = "The email of the service account associated with the instance."
-  type        = string
-  default     = "467144775560-compute@developer.gserviceaccount.com"
-}
-
-variable "zone" {
-  description = "The zone where the Google Compute Engine instance is located."
-  type        = string
-  default     = "us-central1-c"
-}
-
-variable "instance_labels" {
-  description = "A map of labels to assign to the instance."
-  type        = map(string)
-  default     = {
-    goog-ec-src = "vm_add-tf"
-  }
-}
-
-variable "can_ip_forward" {
-  description = "Whether the instance can forward IP packets."
-  type        = bool
-  default     = false
-}
-
-variable "deletion_protection" {
-  description = "Whether to enable deletion protection on the instance."
-  type        = bool
-  default     = false
-}
-
-variable "enable_display" {
-  description = "Whether to enable display on the instance."
-  type        = bool
-  default     = false
+  default     = "PREMIUM"
 }
 
 variable "automatic_restart" {
-  description = "Whether to automatically restart the instance when it is terminated."
+  description = "Whether the instance should be automatically restarted if it crashes."
   type        = bool
   default     = true
 }
 
 variable "on_host_maintenance" {
-  description = "The maintenance behavior for the instance."
+  description = "What happens when the host machine is under maintenance."
   type        = string
   default     = "MIGRATE"
 }
 
 variable "preemptible" {
-  description = "Whether the instance is preemptible."
+  description = "Whether the instance is a preemptible instance."
   type        = bool
   default     = false
 }
@@ -102,20 +70,46 @@ variable "provisioning_model" {
   default     = "STANDARD"
 }
 
-variable "shielded_integrity_monitoring" {
-  description = "Whether to enable integrity monitoring for Shielded VM."
+variable "service_account_email" {
+  description = "The email address of the service account."
+  type        = string
+  default     = "651574577013-compute@developer.gserviceaccount.com"
+}
+
+variable "service_account_scopes" {
+  description = "The scopes for the service account."
+  type        = list(string)
+  default     = [
+    "https://www.googleapis.com/auth/devstorage.read_only",
+    "https://www.googleapis.com/auth/logging.write",
+    "https://www.googleapis.com/auth/monitoring.write",
+    "https://www.googleapis.com/auth/service.management.readonly",
+    "https://www.googleapis.com/auth/servicecontrol",
+    "https://www.googleapis.com/auth/trace.append"
+  ]
+}
+
+variable "enable_integrity_monitoring" {
+  description = "Whether to enable integrity monitoring for the shielded instance."
   type        = bool
   default     = true
 }
 
-variable "shielded_secure_boot" {
-  description = "Whether to enable secure boot for Shielded VM."
+variable "enable_secure_boot" {
+  description = "Whether to enable secure boot for the shielded instance."
   type        = bool
   default     = false
 }
 
-variable "shielded_vtpm" {
-  description = "Whether to enable vTPM for Shielded VM."
+variable "enable_vtpm" {
+  description = "Whether to enable vTPM for the shielded instance."
   type        = bool
   default     = true
 }
+
+variable "zone" {
+  description = "The zone where the instance should be created."
+  type        = string
+  default     = "us-central1-c"
+}
+
